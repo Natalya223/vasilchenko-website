795f952981c6bde56f3cb8a6a4ebeabb640ac12e
Yaroslav Stupkov <yaroslavastupkov@gmail.com>
2026-01-14 00:14:05 +0500

    Clean deploy

--- app/Components/SectionSeparator.tsx
+++ app/Components/SectionSeparator.tsx
@@ -1,5 +1,6 @@
 export default function SectionSeparator({ small=false } : { small? : boolean }) {
   return (
+    // 123
     <div className={small ? "mt-24 sm:mt-52" : "mt-32 sm:mt-72"}></div>
   );
 }
